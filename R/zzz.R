39761af6ad88fa97f1a10c78b31277560d521743
Markus Boenn <markus.boenn@ufz.de>
2016-08-26 16:01:39 +0000

    should pass BUILD now, second try
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/geecc@120513 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,9 @@
 .onAttach <- function(lib,pkg){
 		ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
 		ver <- as.character(ver)
-		packageStartupMessage(paste("geecc", ver, "loaded
+		packageStartupMessage(paste("
+geecc
+", ver, "loaded
 "))
 		}
 .onUnload <- function(libpath){ library.dynam.unload("geecc", libpath) }
